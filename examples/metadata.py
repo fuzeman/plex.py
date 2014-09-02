18a58993bd189db61906f01db87f12af0eb539ac
Dean Gardiner <gardiner91@gmail.com>
2014-09-02 13:46:15 +1200

    Updated [examples/metadata.py]

--- examples/metadata.py
+++ examples/metadata.py
@@ -2,13 +2,16 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 from plex import Plex
-from plex.ext.metadata import Matcher, Metadata
+from plex.ext.metadata import Guid, Matcher, Metadata
 
 
 
 if __name__ == '__main__':
     episode = Metadata.get(3)
     print "episode:", episode
+
+    guid = Guid.parse(episode.guid)
+    print "guid:", guid
 
     identifier = Matcher.process(episode)
     print "identifier:", identifier
