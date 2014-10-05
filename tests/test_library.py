11438944fb3eee74019420f7808398777eaab615
Dean Gardiner <gardiner91@gmail.com>
2014-10-06 10:01:02 +1300

    Added [/library] recently_added() test

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from plex import Plex
 
 from tests.core.helpers import read
@@ -5,6 +7,44 @@ import responses
 
 # Set client configuration defaults
 Plex.configuration.defaults.server(host='mock')
+
+
+@responses.activate
+def test_recently_added():
+    responses.add(
+        responses.GET, 'http://mock:32400/library/recentlyAdded',
+        body=read('fixtures/library/recently_added.xml'), status=200,
+        content_type='application/xml'
+    )
+
+    container = Plex['library'].recently_added()
+    assert container is not None
+
+    items = list(container)
+    assert len(items) == 3
+
+    # Validate 'movie'
+    assert items[0].type == 'movie'
+    assert items[0].title == "TRON: Legacy"
+    assert items[0].year == 2010
+
+    # Validate 'season'
+    assert items[1].type == 'season'
+    assert items[1].title == "Specials"
+    assert items[1].index == 0
+    assert items[1].show.title == "Gold Rush"
+    assert items[1].show.index == 1
+
+    # Validate 'album'
+    assert items[2].type == 'album'
+    assert items[2].title == "Angel Milk"
+    assert items[2].index == 1
+    assert items[2].artist.title == u"Télépopmusik"
+
+    assert [genre.tag for genre in items[2].genres] == [
+        "Electronic",
+        "Electronica"
+    ]
 
 
 @responses.activate
