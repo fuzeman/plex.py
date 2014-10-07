43faea6c4fe37513704664112dfef5a3ef4088fc
Dean Gardiner <gardiner91@gmail.com>
2014-10-07 17:52:25 +1300

    Added [SectionContainer] and [/library/sections] all() tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -66,25 +66,3 @@ def test_recently_added():
         "Electronic",
         "Electronica"
     ]
-
-
-@responses.activate
-def test_sections():
-    responses.add(
-        responses.GET, 'http://mock:32400/library/sections',
-        body=read('fixtures/library/sections.xml'), status=200,
-        content_type='application/xml'
-    )
-
-    container = Plex['library'].sections()
-    assert container is not None
-
-    items = list(container)
-    assert len(items) == 2
-
-    # Validate sections
-    assert items[0].type == 'show'
-    assert items[0].agent == 'com.plexapp.agents.thetvdb'
-
-    assert items[1].type == 'movie'
-    assert items[1].agent == 'com.plexapp.agents.themoviedb'
